323f2b1fd9926ce9d67180fc61e1a8e23e31e462
Sanka <cmbconnection97@gmail.com>
2022-07-03 14:25:56 +0530

    Skin Diseases flutter Application main.dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:imageclassify/Feed.dart';
-import 'package:imageclassify/Menu.dart';
 import 'package:imageclassify/Channel.dart';
 import 'package:imageclassify/Home.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -19,26 +17,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key }) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin{
-
-
+    with SingleTickerProviderStateMixin {
   late TabController _controller;
-
 
   @override
   void initState() {
@@ -52,51 +44,43 @@ class _MyHomePageState extends State<MyHomePage>
     super.dispose();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-       bottomNavigationBar:Container(
-         margin: EdgeInsets.all(20),
-         decoration: BoxDecoration(
-           color: Colors.redAccent,
-           borderRadius: BorderRadius.circular(10),
-         ),
-           child: ClipRRect(
-             borderRadius: BorderRadius.circular(20),
-               child: DefaultTabController(
-                 length: 3,
-                 child : TabBar(
-                   controller: _controller,
-                   unselectedLabelColor: Colors.black,
-                   tabs: [
-                     Tab(
-                       icon: Icon(Icons.home_outlined),
-                       text: "Home",
-                     ),
-                     Tab(
-                       icon: Icon(Icons.feedback_outlined),
-                       text: "Feed",
-                     ),
-                     Tab(
-                       icon: Icon(Icons.contacts_outlined),
-                       text: "Channel",
-                     ),
-                   ],
-                 ),
-               )
-           )
-       ),
+      bottomNavigationBar: Container(
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: DefaultTabController(
+                length: 3,
+                child: TabBar(
+                  controller: _controller,
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.home_outlined),
+                      text: "Home",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.feedback_outlined),
+                      text: "Feed",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.contacts_outlined),
+                      text: "Channel",
+                    ),
+                  ],
+                ),
+              ))),
       body: TabBarView(
-        children: [
-          Menu(),
-          Feed(),
-          Channel()
-        ],
-      controller: _controller,
-    ),
+        children: [Home(), Feed(), Channel()],
+        controller: _controller,
+      ),
     );
   }
 }
